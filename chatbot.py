1528be18e627afcd209fba4e00473024644f6952
Atharva Amritkar <atharvaamritkar@protonmail.com>
2023-02-08 19:34:32 +0530

    testing cors
    Signed-off-by: Atharva Amritkar <atharvaamritkar@protonmail.com>

--- chatbot.py
+++ chatbot.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request, routing
 from mongodb import db_connection
 import openai
 from fastapi.middleware.cors import CORSMiddleware
+
 # Load your API key from an environment variable or secret management service
 openai.api_key = "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"
 
@@ -12,83 +13,85 @@ origins = [
     "http://localhost",
     "http://localhost:3000",
     "http://localhost:8000",
+    "https://citedme.net/reader/paper",
+    "https://citedme.net/",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+
 @app.post("/")
 async def input(request: Request):
-
     request = await request.json()
-    req_prompt = request.get('prompt')
-    userID = request.get('userid')
+    req_prompt = request.get("prompt")
+    userID = request.get("userid")
 
     user_chatbot = db_connection()
 
     list_of_users = []
 
     for x in user_chatbot.find():
-        list_of_users.append(x.get('userid'))
+        list_of_users.append(x.get("userid"))
 
     if userID not in list_of_users:
         print(userID)
-        req_prompt = request.get('prompt')
+        req_prompt = request.get("prompt")
 
         response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt= req_prompt,
-        temperature=0,
-        max_tokens=100,
+            model="text-davinci-003",
+            prompt=req_prompt,
+            temperature=0,
+            max_tokens=100,
         )
 
-        res = response.get('choices')[0].get('text')
+        res = response.get("choices")[0].get("text")
 
-        user_dict = {'userid': userID, 'prompt': "Q) {}, A) {} \n".format(req_prompt, res)}
+        user_dict = {
+            "userid": userID,
+            "prompt": "Q) {}, A) {} \n".format(req_prompt, res),
+        }
 
         user_chatbot.insert_one(user_dict)
 
         return res
 
     else:
-        x = user_chatbot.find({'userid':userID})
-        req_prompt = request.get('prompt')
-        prev_prompt = x[0].get('prompt')
-        
-        new_prompt = prev_prompt + 'Q)' + req_prompt
+        x = user_chatbot.find({"userid": userID})
+        req_prompt = request.get("prompt")
+        prev_prompt = x[0].get("prompt")
+
+        new_prompt = prev_prompt + "Q)" + req_prompt
 
         response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt= new_prompt,
-        temperature=0,
-        max_tokens=100,
+            model="text-davinci-003",
+            prompt=new_prompt,
+            temperature=0,
+            max_tokens=100,
         )
 
-        res = response.get('choices')[0].get('text')
+        res = response.get("choices")[0].get("text")
 
-        user_dict = {'userid': userID, 'prompt': "{}, A) {} \n".format(new_prompt, res)}
-        user_chatbot.delete_one({'userid':userID})
-        user_chatbot.insert_one(user_dict) 
+        user_dict = {"userid": userID, "prompt": "{}, A) {} \n".format(new_prompt, res)}
+        user_chatbot.delete_one({"userid": userID})
+        user_chatbot.insert_one(user_dict)
 
         return res
-
-
-
 
     # print(list_of_users)
 
     response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt= req_prompt,
-    temperature=0,
-    max_tokens=10,
+        model="text-davinci-003",
+        prompt=req_prompt,
+        temperature=0,
+        max_tokens=10,
     )
-                             
+
     # print(response.get('choices')[0].get('text'))
 
     # print(request)
